f45bd9468e86693d244c6e1b1b8cb584cf3fc80c
knack <knack@me.com>
2017-12-13 01:05:34 -0300

    g

--- esppost.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$Temp=$_GET("temperature");
-	$Humidity=$_GET("humidity");
-	$Write="<p>Temp=:".$Temp."Celecius</p><br>"."<p>HUmidity:".$Humidity"%</p>";
-	file_put_contents('index.html', $Write);
-	?>
