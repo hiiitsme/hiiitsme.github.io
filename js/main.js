38536460fcedec60534c275ab8f6805a29ae902a
knack <knack@me.com>
2017-12-23 22:09:55 -0300

    due

--- js/main.js
+++ js/main.js
@@ -5,3 +5,13 @@
   */
 
   // Toggle tranparent navbar when the user scrolls the page
+$(window).scroll(function() {
+  $(".divHiddenAnim").each(function(){
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+     $(this).addClass("divNotHiddenAnim");
+    }
+  });
+});
